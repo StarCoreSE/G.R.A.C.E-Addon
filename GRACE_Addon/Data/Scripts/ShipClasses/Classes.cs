6a6ca4784accb01e16db3bab18b8912ad0e3b25e
CommodoreChet <50426713+CommodoreChet@users.noreply.github.com>
2024-11-05 23:47:35 -0600

    Replaced Test Classes With Prototypes

--- GRACE_Addon/Data/Scripts/ShipClasses/Classes.cs
+++ GRACE_Addon/Data/Scripts/ShipClasses/Classes.cs
@@ -7,19 +7,19 @@ namespace Scripts
     {
         public Parts()
         {
-            AddClassDefinitions(ShipClass1, ShipClass2);
+            AddClassDefinitions(Flagship, Carrier, Cruiser, Destroyer, Corvette, Scout);
         }
 
-        public ClassDefinition ShipClass1 => new ClassDefinition
+        public ClassDefinition Flagship => new ClassDefinition
         {
-            ClassName = "TestClass01",
+            ClassName = "Flagship",
             ClassKey = 11, // Must be Unique per class, cannot be 0 or more than 2 digits
-            PerFactionAmount = 6, // Maximum per Faction
+            PerFactionAmount = 1, // Maximum per Faction
             PerPlayerAmount = 1, // Maximum per Player
-            MaxBlockCount = 6000,
-            MinBlockCount = 500, 
-            MaxClassWeight = 1000000,
-            MinClassWeight = 250000,
+            MaxBlockCount = 15000,
+            MinBlockCount = 0,
+            MaxClassWeight = 40000000,
+            MinClassWeight = 30000000,
             BlacklistedBlocks = new List<string> // Blocks that the Grid cannot have. Placement Attempts will be intercepted, and pastes/prints with these blocks will be in Violation
             {
                 "Battery",
@@ -27,17 +27,85 @@ namespace Scripts
             }
         };
 
-        public ClassDefinition ShipClass2 => new ClassDefinition
+        public ClassDefinition Carrier => new ClassDefinition
         {
-            ClassName = "TestClass02",
-            ClassKey = 12,
-            PerFactionAmount = 6,
-            PerPlayerAmount = 1,
-            MaxBlockCount = 6000,
-            MinBlockCount = 500,
-            MaxClassWeight = 1000000,
-            MinClassWeight = 250000,
-            BlacklistedBlocks = new List<string>
+            ClassName = "Carrier",
+            ClassKey = 12, // Must be Unique per class, cannot be 0 or more than 2 digits
+            PerFactionAmount = 1, // Maximum per Faction
+            PerPlayerAmount = 1, // Maximum per Player
+            MaxBlockCount = 10000,
+            MinBlockCount = 0,
+            MaxClassWeight = 35000000,
+            MinClassWeight = 25000000,
+            BlacklistedBlocks = new List<string> // Blocks that the Grid cannot have. Placement Attempts will be intercepted, and pastes/prints with these blocks will be in Violation
+            {
+                "Battery",
+                "Collector"
+            }
+        };
+
+        public ClassDefinition Cruiser => new ClassDefinition
+        {
+            ClassName = "Cruiser",
+            ClassKey = 13, // Must be Unique per class, cannot be 0 or more than 2 digits
+            PerFactionAmount = 2, // Maximum per Faction
+            PerPlayerAmount = 2, // Maximum per Player
+            MaxBlockCount = 8000,
+            MinBlockCount = 0,
+            MaxClassWeight = 25000000,
+            MinClassWeight = 15000000,
+            BlacklistedBlocks = new List<string> // Blocks that the Grid cannot have. Placement Attempts will be intercepted, and pastes/prints with these blocks will be in Violation
+            {
+                "Battery",
+                "Collector"
+            }
+        };
+
+        public ClassDefinition Destroyer => new ClassDefinition
+        {
+            ClassName = "Destroyer",
+            ClassKey = 14, // Must be Unique per class, cannot be 0 or more than 2 digits
+            PerFactionAmount = 4, // Maximum per Faction
+            PerPlayerAmount = 4, // Maximum per Player
+            MaxBlockCount = 5000,
+            MinBlockCount = 0,
+            MaxClassWeight = 20000000,
+            MinClassWeight = 10000000,
+            BlacklistedBlocks = new List<string> // Blocks that the Grid cannot have. Placement Attempts will be intercepted, and pastes/prints with these blocks will be in Violation
+            {
+                "Battery",
+                "Collector"
+            }
+        };
+
+        public ClassDefinition Corvette => new ClassDefinition
+        {
+            ClassName = "Corvette",
+            ClassKey = 15, // Must be Unique per class, cannot be 0 or more than 2 digits
+            PerFactionAmount = 6, // Maximum per Faction
+            PerPlayerAmount = 6, // Maximum per Player
+            MaxBlockCount = 2500,
+            MinBlockCount = 0,
+            MaxClassWeight = 10000000,
+            MinClassWeight = 5000000,
+            BlacklistedBlocks = new List<string> // Blocks that the Grid cannot have. Placement Attempts will be intercepted, and pastes/prints with these blocks will be in Violation
+            {
+                "Battery",
+                "Collector"
+            }
+        };
+
+        public ClassDefinition Scout => new ClassDefinition
+        {
+            ClassName = "Scout",
+            ClassKey = 16, // Must be Unique per class, cannot be 0 or more than 2 digits
+            PerFactionAmount = 8, // Maximum per Faction
+            PerPlayerAmount = 8, // Maximum per Player
+            MaxBlockCount = 1500,
+            MinBlockCount = 0,
+            MaxClassWeight = 5000000,
+            MinClassWeight = 150000,
+            BlacklistedBlocks = new List<string> // Blocks that the Grid cannot have. Placement Attempts will be intercepted, and pastes/prints with these blocks will be in Violation
             {
                 "Battery",
                 "Collector"
